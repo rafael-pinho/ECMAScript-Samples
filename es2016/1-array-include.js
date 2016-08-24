82c5a7c20929ea979f3fc6eb63bd681c46b9e7e0
Rafael Pinho <rp.rafaelpinho@gmail.com>
2016-08-24 11:21:13 -0300

    exemplo de include com +0 e -0

--- es2016/1-array-include.js
+++ es2016/1-array-include.js
@@ -3,7 +3,12 @@ let numbers = [1, 2, 3, 4, 5];
 console.log(numbers.includes(1))//true
 console.log(numbers.includes(10))//false
 
-//include encontra NaN (diferente do indexOf), e não diferencia +0 e -0
+//include encontra NaN (diferente do indexOf)
 console.log(numbers.includes(NaN));//false
 numbers.push(NaN);
 console.log(numbers.includes(NaN));//true
+
+//include não diferencia +0 e -0
+numbers.push(+0);
+console.log(Object.is(-0, +0)); //false
+console.log(numbers.includes(-0));//true
